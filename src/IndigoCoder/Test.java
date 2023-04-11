bc8dbae91e1e74d7c4954f5ac0de383d62dbfedd
Black-Stringer <hjs40111@gmail.com>
2023-04-11 10:07:44 +0900

    make new class

---
+++ src/IndigoCoder/Test.java
@@ -0,0 +1,12 @@
+package IndigoCoder;
+
+public class Test {
+    private String thisIsProperty = "This is property and this is method";
+    private String thisIsAlsoProperty = "This is also property and this is also method";
+    public void thisIsMethod(){
+        System.out.print(thisIsProperty);
+    }
+    public void thisIsAlsoMethod(){
+        System.out.print(thisIsAlsoProperty);
+    }
+}
